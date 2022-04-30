3f9cec02feb12b09ba978e606d4fe9e19e8c7969
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-04-30 01:57:27 -0500

    installation package of Animations and creation of pokemon card styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import PokemonCards from './PokemonCards';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import PokemonCards from "./PokemonCards";
+import "animate.css";
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-   <PokemonCards />
-);
-
-
+const root = createRoot(document.getElementById("root"));
+root.render(<PokemonCards />);
